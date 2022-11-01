fe6ff6bfe33a76326e11b145e9d2c265c97ddbbd
RuthMaria <ruth-15rms@hotmail.com>
2022-10-31 22:09:22 -0300

    add lib zod

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-
 import { Play } from 'phosphor-react';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod'; // '@hookform/resolvers' faz a integração do react-hook-form com o zod, vc pode escolher outro validador também (joi, yup)
+import * as zod from 'zod';
 
 import {
   CountdownContainer,
@@ -12,16 +14,38 @@ import {
   TaskInput,
 } from './styles';
 
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos.')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos.'),
+});
+
 export const Home: React.FC = () => {
+  const { register, handleSubmit, watch, formState } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  });
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data);
+  }
+
+  console.log(formState.errors); // mostra todos os erros do formulário
+
+  const task = watch('task'); // pega o valor atual do input task
+  const isSubmitDisable = !task;
+
   return (
     <HomeContainer>
-      <form>
+      <form onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register('task')} // registra os campos que o form terá
           />
 
           <datalist id="task-suggestions">
@@ -39,6 +63,7 @@ export const Home: React.FC = () => {
             step={5} /* faz com que seja adicionado de 5 em 5 */
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })} // por padrão o valor digitado no input será retornado pelo register como uma string, então o valueAsNumber força a ser um number
           />
 
           <span>minutos.</span>
@@ -52,7 +77,7 @@ export const Home: React.FC = () => {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisable} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
